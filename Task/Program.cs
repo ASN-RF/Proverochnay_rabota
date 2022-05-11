ae0f0cef42e5a1939ff4cbdeb041ed8d36a112ad
Gr_n RF <gr.nrfinfo@gmail.com>
2022-05-11 21:24:17 +0600

    added PrintArrayVar  in Task

--- Task/Program.cs
+++ Task/Program.cs
@@ -6,3 +6,14 @@
                     ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
                     ["1234", "1567", "-2", "computer science"] -> ["-2"]
                     ["Russia", "Denmark", "Kazan"] -> []. */
+string[] ArrayVar0 = new string[] { };
+string[] ArrayVar1 = new string[] { "hello", "2", "world", ":-)" };
+string[] ArrayVar2 = new string[] { "1234", "1567", "-2", "computer science" };
+string[] ArrayVar3 = new string[] { "Russia", "Denmark", "Kazan" };
+void PrintArrayVar(string[] array0) // ПЕЧАТЬ Заданных массивов в примере
+{
+    Console.Write("['");
+    var str = string.Join("', '", array0);
+    Console.Write(str);
+    Console.Write("'] --> [");
+}
